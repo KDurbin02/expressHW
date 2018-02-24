1a66fcddf00ec30c5d6606fb9cff8b5d0ed48fdc
Kyla <kyladurbin15@gmail.com>
2018-02-24 10:34:41 -0500

    added a lot

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,37 @@
+// ALL POSSIBLE FRIENDS DATA //
+
+var friends = [{
+    "name": "Eden",
+    "photo": "images/eden.jpg",
+    "scores": [5, 2, 3, 5, 5, 3, 2, 5, 1, 1]
+},
+{
+    "name": "b",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [3, 2, 4, 1, 3, 4, 2, 3, 4, 2]
+},
+{
+    "name": "c",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [1, 2, 4, 3, 5, 4, 2, 5, 2, 3]
+},
+{
+    "name": "d",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [2, 5, 2, 1, 5, 2, 2, 5, 3, 1]
+},
+{
+    "name": "e",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [2, 5, 2, 4, 1, 1, 2, 2, 4, 4]
+},
+{
+    "name": "f",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [3, 4, 2, 4, 3, 2, 2, 2, 4, 3]
+},
+]
+
+
+//export friends here//
+module.exports = friends;
